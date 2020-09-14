276936295544fc8af121022e50d28a07608b530a
kowshikkuri <ratulkuri@gmail.com>
2020-09-15 01:51:00 +0600

    Academic Page

--- src/pages/academic.js
+++ src/pages/academic.js
@@ -1,13 +1,37 @@
 import React from "react"
 import Container from "../components/container"
 import AcademicStyles from "../styles/pages/academic.module.css"
+import { FaBuilding } from "react-icons/fa";
+
 
 export default function Academic() {
   return (
     <Container>
         <div className="row">
             <div className="col">
-                <h2 className={AcademicStyles.h2}>Academic</h2>
+              <div className="d-flex j-c-center a-items-center" style={{ minHeight: "calc(100vh - 106px)"}}>
+                <ul className={ AcademicStyles.resume }>
+                  <li>
+                    <FaBuilding className={AcademicStyles.icon} />
+                    <h6>Lakshmipur Adarsha Samad Government High School</h6>
+                    <p><strong>Secondary School Certificate</strong></p>
+                    <p><small>2005–2006 | Science</small></p>
+                  </li>
+                  <li>
+                    <FaBuilding className={AcademicStyles.icon} />
+                    <h6>Lakshmipur Government College</h6>
+                    <p><strong>Higher Secondary Certificate</strong></p>
+                    <p><small>2007–2008 | Science</small></p>
+                    <p><small /></p>
+                  </li>
+                  <li>
+                    <FaBuilding className={AcademicStyles.icon} />
+                    <h6>Creative IT Institute</h6>
+                    <p><strong>Shortcourse</strong></p>
+                    <p><small>2018 | Web Developement</small></p>
+                  </li>
+                </ul>
+              </div>
             </div>
         </div>
     </Container>
